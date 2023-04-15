4119f7f3f9547d765ea1e514a87cf91734d90ef2
lucas.santos <lucas.nepomuceno1999@gmail.com>
2023-04-14 23:48:39 -0300

    feat(pet): added default values for future feature

---
+++ pet/src/main/java/com/lnsantos/pet/core/PetColors.kt
@@ -0,0 +1,21 @@
+package com.lnsantos.pet.core
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFFF44174)
+val PrimaryVariant = Primary
+val Secondary = Color(0xFFFA7E61)
+val SecondaryVariant = Secondary
+val Terliary = Color(0xFFFEE1C7)
+val Background = Color(0xFF4C1E4F)
+val Surface = Background
+val Error = Color(0xFF880202)
+
+val PrimaryDark = Color(0xFF992949)
+val PrimaryVariantDark = PrimaryDark
+val SecondaryDark = Color(0xFFAF6250)
+val SecondaryVariantDark = SecondaryDark
+val TerliarydDark = Color(0xFFFEE1C7)
+val BackgroundDark = Color(0xFF300E32)
+val SurfaceDark = BackgroundDark
+val ErrorDark = Color(0xFFDF0202)
